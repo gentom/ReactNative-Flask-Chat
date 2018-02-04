c64f86acc5dac9fb13d4609fa19ab766fdecef35
gentom <gento.m.developer@gmail.com>
2018-02-05 00:51:04 +0900

    add send(send message to myself)/read functions for messages

--- chat-client/App.js
+++ chat-client/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       input: '',
+      msgs: [],
     };
   }
+
+  onMsgSend(){
+    const { input, msgs } = this.state;
+    msgs.push(input);
+  }
+
   render() {
+    const { msgs } = this.state;
     return (
       <View style={styles.container}>
-        <Text>{this.state.input}</Text>
+        <FlatList>
+          data = {msgs}
+          renderItem = {({item}) => <Text>{item}</Text>}
+        </FlatList>
         <TextInput onChangeText={(val) => this.setState({input: val})} />
-        <Button title='send' onPress={() => {}} />
+        <Button title='send' onPress={() => this.onMsgSend()} />
       </View>
     );
   }
