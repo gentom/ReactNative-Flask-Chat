79c4854f4a6f8c8b2d6b3073769b5baaafff8709
gentom <gento.m.developer@gmail.com>
2018-02-05 20:19:29 +0900

    add axios, update App.js(add onLogin function)

--- chat-client/App.js
+++ chat-client/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import axios from 'axios';
+
+const serverURL = 'http://localhost:8668';
+const http = axios.create({
+  baseURL: serverURL,
+});
 
 export default class App extends React.Component {
   constructor(props){
@@ -10,15 +16,31 @@ export default class App extends React.Component {
     };
   }
 
+  onLogin(){
+    const { isLoggedIn } = this.state;
+    if(!isLoggedIn){
+
+    }
+  }
+
   onMsgSend(){
     const { input, msgs } = this.state;
     msgs.push(input);
   }
 
+
   render() {
-    const { msgs } = this.state;
+    const { msgs, isLoggedIn } = this.state;
     return (
       <View style={styles.container}>
+
+        <View>
+          <Text>Login</Text>
+          <TextInput style={{ backgroundColor: '#ededed' }} onChangeText={(val) => this.setState({username: val})} />
+          <Button title='Login' onPress={() => this.onLogin()} />
+          <Text>Online Status: {isLoggedIn ? 'Online' : 'Offline'}</Text>
+        </View>
+
         <FlatList>
           data = {msgs}
           renderItem = {({item}) => <Text>{item}</Text>}
